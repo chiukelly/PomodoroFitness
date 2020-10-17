93728d9c2053398817ff0501a1226695ed9a3832
Kelly Chiu <chiukelly3@gmail.com>
2020-10-17 12:08:49 -0500

    Initial Commit

---
+++ stepcity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stepcity
+//
+//  Created by Kelly Chiu on 10/17/20.
+//  Copyright © 2020 convergent health bt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
