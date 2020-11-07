bbb79bac5d6c72512dea70ebfd5d934ddbdedc76
Kelly Chiu <chiukelly3@gmail.com>
2020-11-07 11:56:33 -0600

    login/signup

--- stepcity/Signup-Login/LoginViewController.swift
+++ stepcity/Signup-Login/LoginViewController.swift
@@ -11,6 +11,10 @@ import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var title1Text: UILabel!
+    
+    @IBOutlet weak var title2Text: UILabel!
+    
     @IBOutlet weak var emailText: UITextField!
     
     @IBOutlet weak var passwordText: UITextField!
@@ -33,31 +37,54 @@ class LoginViewController: UIViewController {
         // Hide error label
         errorLabel.alpha = 0
         
-        // Style the elements
+        // title1Text.font = UIFont(name: "PixelOperatorSC-Bold", size:60)
     }
+    
+    // Check the fields and validate that the data is correct
+    func validateFields() -> String? {
+        // Check that all fields are filled in
+        if emailText.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||   passwordText.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields"
+        }
+        return nil
+    }
+    
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
     @IBAction func loginTapped(_ sender: Any) {
         
         // TODO: Validate Text Fields
+        let error = validateFields()
         
-        // Create cleaned versions of the text fields
-        let email = emailText.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let password = passwordText.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if error != nil {
+            // Something wrong with the fields, show error message
+            showError(error!)
+        }
+        else {
+            // Create cleaned versions of the text fields
+            let email = emailText.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordText.text!.trimmingCharacters(in: .whitespacesAndNewlines)
 
         
-        // Signing in the user
-        Auth.auth().signIn(withEmail: email, password: password) {
+            // Signing in the user
+            Auth.auth().signIn(withEmail: email, password: password) {
             (result, error) in
             
-            if error != nil {
-                // Couldn't sign in
-                self.errorLabel.text = error!.localizedDescription
-                self.errorLabel.alpha = 1
-            }
-            else {
-                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                if error != nil {
+                    // Couldn't sign in
+                    self.errorLabel.text = error!.localizedDescription
+                    self.errorLabel.alpha = 1
+                }
+                else {
+                    let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
                 
-                self.view.window?.rootViewController = homeViewController
-                self.view.window?.makeKeyAndVisible()
+                    self.view.window?.rootViewController = homeViewController
+                    self.view.window?.makeKeyAndVisible()
+                }
             }
         }
     }
