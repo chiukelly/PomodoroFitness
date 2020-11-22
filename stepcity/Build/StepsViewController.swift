3c435cdcc47e579cae94b2bea32db8caa861734b
Kelly Chiu <chiukelly3@gmail.com>
2020-11-22 00:39:51 -0600

    build

--- stepcity/Build/StepsViewController.swift
+++ stepcity/Build/StepsViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class StepsViewController: UIViewController {
 
+    @IBOutlet weak var editBuildingButton: UIButton!
+    @IBOutlet weak var startSteppingButton: UIButton!
+    
     var count = 100
    
     @IBOutlet weak var goalStepsText: UILabel!
@@ -19,6 +22,16 @@ class StepsViewController: UIViewController {
         super.viewDidLoad()
         goalStepsText.layer.cornerRadius = 10.0
         goalStepsText.clipsToBounds = true
+        
+        editBuildingButton.backgroundColor = UIColor(red: 95.0, green: 108.0, blue: 227.0, alpha: 1.0)
+        editBuildingButton.layer.cornerRadius = 30
+        editBuildingButton.layer.borderWidth = 1
+        editBuildingButton.layer.borderColor = UIColor.lightGray.cgColor
+        
+        startSteppingButton.backgroundColor = UIColor(red: 95.0, green: 108.0, blue: 227.0, alpha: 1.0)
+        startSteppingButton.layer.cornerRadius = 30
+        startSteppingButton.layer.borderWidth = 1
+        startSteppingButton.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     @IBAction func increaseSteps(_ sender: Any) {
